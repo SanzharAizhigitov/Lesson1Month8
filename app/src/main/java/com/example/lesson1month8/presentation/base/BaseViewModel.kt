752c2676f6f578277525560d3b2f2c0232cb66da
SanzharAizhigitov <ajzigitovsanzar@gmail.com>
2023-05-16 19:21:54 +0600

    комитнул

---
+++ app/src/main/java/com/example/lesson1month8/presentation/base/BaseViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.lesson1month8.presentation.base
+
+import android.widget.Toast
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.lifecycle.viewModelScope
+import com.example.lesson1month8.domain.utils.Resource
+import com.example.lesson1month8.presentation.ui.utils.UIState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModel: ViewModel() {
+
+    protected fun <T> Flow<Resource<T>>.collectData(
+        _state: MutableStateFlow<UIState<T>>
+    ) {
+        viewModelScope.launch {
+            viewModelScope.launch {
+                this@collectData.collect() { res ->
+                    when (res) {
+                        is Resource.Error -> {
+                            _state.value = UIState.Error(res.message!!)
+                        }
+
+                        is Resource.Loading -> {
+                            _state.value = UIState.Loading()
+                        }
+
+                        is Resource.Success -> {
+                            if (res.data != null)
+                                _state.value = UIState.Success(res.data)
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
